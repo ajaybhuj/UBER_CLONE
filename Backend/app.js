9a1ae7705091460c9f2faec10ca6e3b76111d6dd
ajaybhuj24 <ajaybhuj2456.com>
2024-12-28 18:57:26 +0545

    initializing the git resp

---
+++ Backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+module.exports = app;
